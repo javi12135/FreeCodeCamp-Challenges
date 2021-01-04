6c46aef57cbdcac76ffb20ebea841f38fc9f9724
javi12135 <javi12135@gmail.com>
2021-01-04 01:00:52 +0100

    Completed exercise 4

---
+++ Scientific Computing with Python/Exercises/02-04_EX.py
@@ -0,0 +1,15 @@
+#1
+width = 17
+width = width//2
+print(width)
+print(type(width))
+#2
+width = 17
+width = width/2.0
+print(width)
+print(type(width))
+#3
+height = 12.0
+height = height/3
+print(height)
+print(type(height))
