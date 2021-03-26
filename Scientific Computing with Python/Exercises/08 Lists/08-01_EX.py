bb3cf40d31d87eb9d8acfc0d7291b6c9e1317517
Jav12135 <javi12135@gmail.com>
2021-03-26 22:18:27 +0100

    Completed exercises 08-01, 08-02 and 08-03

--- Scientific Computing with Python/Exercises/08 Lists/08-01_EX.py
+++ Scientific Computing with Python/Exercises/08 Lists/08-01_EX.py
@@ -1,1 +1,16 @@
 #Exercise 1: Write a function called chop that takes a list and modifies it, removing the first and last elements, and returns None. Then write a function called middle that takes a list and returns a new list that contains all but the first and last elements
+
+def chop(list):
+    list.pop(0)
+    list.pop(len(list)-1)
+    print(list)
+
+def middle(list):
+    return list[1:len(list)-1]
+
+list = [0,1,2,3,4,5,6]
+chop(list)
+
+list = [0,1,2,3,4,5,6]
+list = middle(list)
+print(list)
--- Scientific Computing with Python/Exercises/08 Lists/08-03_EX.py
+++ Scientific Computing with Python/Exercises/08 Lists/08-03_EX.py
@@ -1,1 +1,9 @@
 #Exercise 3: Rewrite the guardian code in the above example without two if statements. Instead, use a compound logical expression using the or logical operator with a single if statement
+
+fhand = open('mbox-short.txt')
+count = 0
+for line in fhand:
+    words = line.split()
+    # print('Debug:', words)
+    if len(words) < 3 or words[0] != 'From' : continue
+    print(words[2])
